9da068f0a81cd07a90abc8ae9b14add38c99929b
Liubomyr <strukliubomyr@gmail.com>
2017-12-17 12:51:00 +0200

    Fix validate function

--- main.js
+++ main.js
@@ -27,16 +27,22 @@ input4.type = 'submit';
 input4.value = 'Validate Me';
 document.getElementsByName('login')[0].appendChild(input4);
 
+/*Validate Event*/
 
 form.addEventListener('submit', validateForm);
 
 function validateForm() {
     
  event.preventDefault();
+console.log(validateAge(input.value));
+ if(validateAge(input.value) && validateUserName(input2.value) && validateData(input3.value))
+ alert("Everything is fine");
+ else alert("Incorrect input!");
+
+/*Change background-color if input is incorrect*/
 
  input.onblur = function(){
-
-if(isNaN(input.value)||input.value<=0)
+if(validateAge() == false)
     input.className = 'error';
 }
 
@@ -47,7 +53,7 @@ input.onfocus = function(){
 
 
 input2.onblur = function(){
-if(input2.value.indexOf('user_') != 0)
+if(validateUserName() == false)
     input2.className = 'error';
 }
 
@@ -58,20 +64,43 @@ input2.onfocus = function(){
 }
 
 input3.onblur = function(){
-    if(input3.value.length != 10) input3.className = 'error';
-    var check=/([0-9][0-9]\/){2}[0-9]{4}/
-if(check.test(input3.value)){
-    if( !((input3.value[0]+input3.value[1])<=31 && (input3.value[3]+input3.value[4])<=12 ))
+    if(validateData() == false)
     input3.className = 'error';
-    else return true;
 }
-else alert('Enter date in format dd/mm/yyyy');
 
-}
 
 input3.onfocus = function(){
     if(input3.className == 'error')
     input3.className = '';
 }
 
+}
+
+
+
+/*Validate Function*/
+
+function validateAge(){
+    if(isNaN(input.value)||input.value<=0)
+    return false;
+     else return true;
+}
+
+
+function validateUserName(){
+    if(input2.value.indexOf('user_') == 0)
+    return true;
+    else return false;
+}
+
+
+function validateData(){
+    if(input3.value.length != 10)return false;
+    var check=/([0-9][0-9]\/){2}[0-9]{4}/
+    if(check.test(input3.value)){
+        if((input3.value[0]+input3.value[1])<=31 && (input3.value[3]+input3.value[4])<=12 )
+        return true;
+        else return false;
+}
+else alert('Enter date in format dd/mm/yyyy');
 }
