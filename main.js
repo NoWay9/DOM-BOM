a1ac2e09f55ee8c4b723e360f0da57a5b81a2c03
Liubomyr <strukliubomyr@gmail.com>
2018-01-09 16:33:02 +0200

    Fix validation function

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ form.addEventListener('submit', validateForm);
 function validateForm() {
     
  event.preventDefault();
-console.log(validateAge(input.value));
+console.log(input3.value);
  if(validateAge(input.value) && validateUserName(input2.value) && validateData(input3.value))
  alert("Everything is fine");
  else alert("Incorrect input!");
@@ -42,36 +42,36 @@ console.log(validateAge(input.value));
 /*Change background-color if input is incorrect*/
 
  input.onblur = function(){
-if(validateAge() == false)
-    input.className = 'error';
+if(validateAge(input.value) == false)
+    input.classList.add('error');
 }
 
 input.onfocus = function(){
     if(input.className == 'error')
-    input.className = '';
+    input.classList.remove('error');
 }
 
 
 input2.onblur = function(){
-if(validateUserName() == false)
-    input2.className = 'error';
+if(validateUserName(input2.value) == false)
+    input2.classList.add('error');
 }
 
 
 input2.onfocus = function(){
     if(input2.className == 'error')
-    input2.className = '';
+    input2.classList.remove('error');
 }
 
 input3.onblur = function(){
-    if(validateData() == false)
-    input3.className = 'error';
+    if(validateData(input3.value) == false)
+    input3.classList.add('error');
 }
 
 
 input3.onfocus = function(){
-    if(input3.className == 'error')
-    input3.className = '';
+    if(input3.className =='error')
+    input3.classList.remove('error');
 }
 
 }
@@ -80,25 +80,25 @@ input3.onfocus = function(){
 
 /*Validate Function*/
 
-function validateAge(){
-    if(isNaN(input.value)||input.value<=0)
+function validateAge(value){
+    if(isNaN(value)||value<=0)
     return false;
      else return true;
 }
 
 
-function validateUserName(){
-    if(input2.value.indexOf('user_') == 0)
+function validateUserName(value){
+    if(value.indexOf('user_') == 0)
     return true;
     else return false;
 }
 
 
-function validateData(){
-    if(input3.value.length != 10)return false;
+function validateData(value){
+    if(value.length != 10)return false;
     var check=/([0-9][0-9]\/){2}[0-9]{4}/
-    if(check.test(input3.value)){
-        if((input3.value[0]+input3.value[1])<=31 && (input3.value[3]+input3.value[4])<=12 )
+    if(check.test(value)){
+        if((value[0]+value[1])<=31 && (value[3]+value[4])<=12 )
         return true;
         else return false;
 }
