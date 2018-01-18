b84e05a376e7c1f82c04b79920304f0183c349d4
Liubomyr <strukliubomyr@gmail.com>
2018-01-18 21:14:38 +0200

    Add placeholder and change variable input name

--- main.js
+++ main.js
@@ -4,75 +4,76 @@ form.action = 'google.com';
 
 document.body.appendChild(form);
 
+var inputAge = document.createElement('input');
+inputAge.type = 'text';
+inputAge.setAttribute('placeholder', 'age');
+inputAge.name = 'age';
 
-var input = document.createElement('input');
-input.type = 'text';
-input.name = 'age';
-
-document.getElementsByName('login')[0].appendChild(input);
-
-
-var input2 = input.cloneNode(true);
-input2.name = 'username';
-input.parentNode.appendChild(input2);
+document.getElementsByName('login')[0].appendChild(inputAge);
 
 
-var input3 = input.cloneNode(true);
-input3.name = 'date';
-input.parentNode.appendChild(input3);
+var inputUserName = inputAge.cloneNode(true);
+inputUserName.name = 'username';
+inputUserName.setAttribute('placeholder', 'username');
+inputAge.parentNode.appendChild(inputUserName);
 
 
-var input4 = document.createElement('input');
-input4.type = 'submit';
-input4.value = 'Validate Me';
-document.getElementsByName('login')[0].appendChild(input4);
+var inputDate = inputAge.cloneNode(true);
+inputDate.name = 'date';
+inputDate.setAttribute('placeholder', 'date');
+inputAge.parentNode.appendChild(inputDate);
+
+
+var inputSubmit = document.createElement('input');
+inputSubmit.type = 'submit';
+inputSubmit.value = 'Validate Me';
+document.getElementsByName('login')[0].appendChild(inputSubmit);
 
 /*Validate Event*/
 
 form.addEventListener('submit', validateForm);
 
 function validateForm() {
-    
- event.preventDefault();
-console.log(input3.value);
- if(validateAge(input.value) && validateUserName(input2.value) && validateData(input3.value))
- alert("Everything is fine");
- else alert("Incorrect input!");
 
-/*Change background-color if input is incorrect*/
+  event.preventDefault();
+  if (validateAge(inputAge.value) && validateUserName(inputUserName.value) && validateData(inputDate.value))
+    alert("Everything is fine");
+  else alert("Incorrect input!");
 
- input.onblur = function(){
-if(validateAge(input.value) == false)
-    input.classList.add('error');
-}
+  /*Change background-color if input is incorrect*/
 
-input.onfocus = function(){
-    if(input.className == 'error')
-    input.classList.remove('error');
-}
+  inputAge.onblur = function () {
+    if (validateAge(inputAge.value) == false)
+      inputAge.classList.add('error');
+  }
 
-
-input2.onblur = function(){
-if(validateUserName(input2.value) == false)
-    input2.classList.add('error');
-}
+  inputAge.onfocus = function () {
+    if (inputAge.className == 'error')
+      inputAge.classList.remove('error');
+  }
 
 
-input2.onfocus = function(){
-    if(input2.className == 'error')
-    input2.classList.remove('error');
-}
-
-input3.onblur = function(){
-    if(validateData(input3.value) == false)
-    input3.classList.add('error');
-}
+  inputUserName.onblur = function () {
+    if (validateUserName(inputUserName.value) == false)
+      inputUserName.classList.add('error');
+  }
 
 
-input3.onfocus = function(){
-    if(input3.className =='error')
-    input3.classList.remove('error');
-}
+  inputUserName.onfocus = function () {
+    if (inputUserName.className == 'error')
+      inputUserName.classList.remove('error');
+  }
+
+  inputDate.onblur = function () {
+    if (validateData(inputDate.value) == false)
+      inputDate.classList.add('error');
+  }
+
+
+  inputDate.onfocus = function () {
+    if (inputDate.className == 'error')
+      inputDate.classList.remove('error');
+  }
 
 }
 
@@ -80,27 +81,27 @@ input3.onfocus = function(){
 
 /*Validate Function*/
 
-function validateAge(value){
-    if(isNaN(value)||value<=0)
+function validateAge(value) {
+  if (isNaN(value) || value <= 0)
     return false;
-     else return true;
+  else return true;
 }
 
 
-function validateUserName(value){
-    if(value.indexOf('user_') == 0)
+function validateUserName(value) {
+  if (value.indexOf('user_') == 0)
     return true;
+  else return false;
+}
+
+
+function validateData(value) {
+  if (value.length != 10) return false;
+  var check = /([0-9][0-9]\/){2}[0-9]{4}/
+  if (check.test(value)) {
+    if ((value[0] + value[1]) <= 31 && (value[3] + value[4]) <= 12)
+      return true;
     else return false;
-}
-
-
-function validateData(value){
-    if(value.length != 10)return false;
-    var check=/([0-9][0-9]\/){2}[0-9]{4}/
-    if(check.test(value)){
-        if((value[0]+value[1])<=31 && (value[3]+value[4])<=12 )
-        return true;
-        else return false;
-}
-else alert('Enter date in format dd/mm/yyyy');
+  }
+  else alert('Enter date in format dd/mm/yyyy');
 }
